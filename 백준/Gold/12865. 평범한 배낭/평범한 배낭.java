33db82e695ca37863d693c5d3fa297a20bb98250
Dongmin Kim <dmk94@naver.com>
2025-04-07 17:21:56 +0900

    [Gold V] Title: 평범한 배낭, Time: 136 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,35 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// 평범한 배낭
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken()); // 배낭 개수
+        int k = Integer.parseInt(st.nextToken()); // 버틸 수 있는 무게
 
-        int[][] item = new int[n + 1][2];
-        for (int i = 1; i <= n; i++) {
+        int[][] items = new int[n][2];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            item[i][0] = Integer.parseInt(st.nextToken());
-            item[i][1] = Integer.parseInt(st.nextToken());
+
+            int w = Integer.parseInt(st.nextToken()); // 물건의 무게
+            int v = Integer.parseInt(st.nextToken()); // 물건의 가치
+
+            items[i][0] = w;
+            items[i][1] = v;
         }
 
-        int[][] dp = new int[n + 1][k + 1];
-
-        for (int i = 1; i <= k; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[j][i] = dp[j - 1][i];
-                if (i - item[j][0] >= 0) {
-                    dp[j][i] = Math.max(dp[j - 1][i], item[j][1] + dp[j - 1][i - item[j][0]]);
-                }
+        int[] dp = new int[k + 1];
+        
+        for (int[] item : items) {
+            for (int w = k; w >= item[0]; w--) {
+                dp[w] = Math.max(dp[w], dp[w - item[0]] + item[1]);
             }
         }
-
-        System.out.println(dp[n][k]);
+        
+        System.out.println(dp[k]);
     }
 }
